bf859fb12aeaeaaf3059d10ea31d4a415370c7a9
Anne Saber <iarabejo3@gmail.com>
2022-04-03 11:44:41 -0500

    Initial route files with express and nodemon dependencies added.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+
