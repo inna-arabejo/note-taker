a2f80f5f6c82ea969c633ffa2453263691684337
Anne Saber <iarabejo3@gmail.com>
2022-04-03 11:59:33 -0500

    Added middleware for parsing json and urlencoded data.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
-const app = express();
+const router = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
+router.use(express.urlencoded({ extended: true }));
+router.use(express.static('public'));
+router.use(express.json());
 
-
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
