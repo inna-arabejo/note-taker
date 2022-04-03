a9c52d694143cde4c03e513358b90bef2a1bc846
Anne Saber <iarabejo3@gmail.com>
2022-04-03 16:47:39 -0500

    Updated api routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,29 +2,43 @@ const fs = require('fs');
 const uuid = require('uuid');
 const router = require('express').Router();
 
-// GET Route 
+// GET Route     // api/notes
 router.get('/notes', (req, res) => {
-  const data = fs.readFileSync('./db/db.json');
-  res.json(JSON.parse(data));
+  fs.readFileAsync('./db/db.json', 'utf8').then(function(data) {
+    notes = [].concat(JSON.parse(data))
+    res.json(notes);
+  });
 })
 
 // POST Route 
 router.post('/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-  const addNote = req.body;
-  addNote.id = uuid.v4();
-  notes.push(addNote);
-  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-  res.json(notes);
-})
+  const note = req.body;
+  readFileAsync('./db/db.json', 'utf8').then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    note.id = notes.length + 1
+    notes.push(note);
+  }).then(function(data) {
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+  })
+});
 
 // DELETE Route
 router.delete('/notes/:id', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-  const deleteNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-  fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
-
-  res.json(deleteNote);
+  const deleteID = parseInt(req.params.id);
+  readFileAsync('./db/db.json', 'utf8').then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNotes = []
+    for(let i = 0; i < notes.length; i++) {
+      if(newNotes !== notes[i].id) {
+        newNotes.push(notes[i])
+      }
+    }
+    return newNotes
+  }).then(function(notes) {
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.send('Successfully Saved!');
+  })
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const api = require('./routes/apiRoutes');
-const app = express();
+const router = express();
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+router.use(express.static('public'));
+router.use(express.json());
 
-app.use('/api', api);
-app.use('/', htmlRoutes);
+router.use('/api', api);
+router.use('/', htmlRoutes);
 
-app.listen(PORT, () =>
+router.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
