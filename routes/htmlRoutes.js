ddb10a459019953cc57562dec1dce2f209fc9313
Anne Saber <iarabejo3@gmail.com>
2022-04-03 18:05:16 -0500

    npm installed packages in json file.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const uuid = require('uuid');
 const path = require('path');
 const router = require('express').Router();
+const util = require('util');
 
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 router.get('*', (req, res) => {
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const api = require('./routes/apiRoutes');
 const router = express();
-const util = require('util');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid');
 const PORT = process.env.PORT || 3001;
+const util = require('util');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -18,6 +19,7 @@ router.use(express.json());
 router.use('/api', api);
 router.use('/', htmlRoutes);
 
+// Server listening 
 router.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
